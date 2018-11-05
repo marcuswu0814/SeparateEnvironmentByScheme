35eb67c4b5a917da9285e07186dd84a6acc0031b
Marcus Wu <marcus.wu0814@gmail.com>
2018-11-05 19:55:45 +0800

    Initial Commit

---
+++ SeparateEnvironment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeparateEnvironment
+//
+//  Created by Marcus Wu on 2018/11/5.
+//  Copyright © 2018 Marcus Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
